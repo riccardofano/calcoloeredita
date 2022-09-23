d4647dc165a05e38e6a5839a1f8032635902b85e
riccardofano <rcr.fano@gmail.com>
2022-09-23 08:52:41 +0200

    add page links

--- templates/Main.tsx
+++ templates/Main.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { Dispatch, FormEvent, SetStateAction, useState } from 'react'
 import RelativesForm from '../components/RelativesForm'
 import RelativesList from '../components/RelativesList'
@@ -14,17 +15,31 @@ function Main({ isEditing, setIsEditing, inheritance, onSubmit }: MainProps) {
   const [selectedId, setSelectedId] = useState('0')
 
   return (
-    <main className="px-8 py-16 mx-auto max-w-prose">
-      <MoneyProvider>
-        {isEditing ? (
-          <form className="space-y-4" onSubmit={onSubmit}>
-            <RelativesForm id={selectedId} setSelectedId={setSelectedId} onSubmit={onSubmit}></RelativesForm>
-          </form>
-        ) : (
-          <RelativesList inheritance={inheritance} setEditing={setIsEditing} />
-        )}
-      </MoneyProvider>
-    </main>
+    <div className="px-8 py-16 space-y-8 mx-auto max-w-prose">
+      <header>
+        <nav>
+          <ul className="flex justify-between text-blue-400 underline">
+            <li>
+              <Link href="/">Calcolo eredità</Link>
+            </li>
+            <li>
+              <Link href="/patrimonio">Calcolo patrimonio</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <MoneyProvider>
+          {isEditing ? (
+            <form className="space-y-4" onSubmit={onSubmit}>
+              <RelativesForm id={selectedId} setSelectedId={setSelectedId} onSubmit={onSubmit}></RelativesForm>
+            </form>
+          ) : (
+            <RelativesList inheritance={inheritance} setEditing={setIsEditing} />
+          )}
+        </MoneyProvider>
+      </main>
+    </div>
   )
 }
 
