61f7931a4960769ebff61067695b2e55ad59104b
Riccardo Fano <rcr.fano@gmail.com>
2024-02-05 20:42:37 +0100

    Format inheritance endpoint

--- pages/api/inheritance.ts
+++ pages/api/inheritance.ts
@@ -2,9 +2,14 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { calculateInheritance } from '../../core/inheritance'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST') return res.status(405).send({ error: 'Method not allowed' })
-  if (req.headers['content-type'] !== 'application/json')
+  if (req.method !== 'POST') {
+    return res.status(405).send({ error: 'Method not allowed' })
+  }
+  if (req.headers['content-type'] !== 'application/json') {
     return res.status(400).send({ error: 'Content type not allowed' })
+  }
+
+  // TODO: Don't require sender to set back pointers for people, only the relatives array
 
   try {
     const result = calculateInheritance(req.body)
