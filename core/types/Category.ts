0b0a3170f3bcffffadef93a7b17edf3182a80e22
Riccardo Fano <rcr.fano@gmail.com>
2024-02-07 17:40:51 +0100

    WIP

--- core/types/Category.ts
+++ core/types/Category.ts
@@ -1,4 +1,4 @@
-export const categoryNames = ['children', 'spouse', 'ascendants', 'bilateral', 'unilateral', 'others'] as const
+export const categoryNames = ['root', 'children', 'spouse', 'ascendants', 'bilateral', 'unilateral', 'others'] as const
 export type CategoryName = typeof categoryNames[number]
 export type Categories = Record<CategoryName, boolean>
 export type CategoryPeople<T> = { [key in CategoryName]: Array<T> }
